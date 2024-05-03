483ba12cf2144b74ba36dab73e613a228c4a9690
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-05-03 17:17:28 -0300

    Criando as rotas de GET e POST para o cadastro

--- app.py
+++ app.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 app.secret_key = "capivara"
 
+@app.route("/cadastrar")
+def pg_cadasatrar_form():
+    return render_template("cadastrar-login.html")
+
+@app.route("/cadastrar", methods=["POST"])
+def pg_cadastro():
+    if session.get("usuario","erro") == "Autenticado":
+        return render_template("cadastrar-login.html")
+    else:
+        return render_template("cadastrar-login.html")
+
 app.run(debug=True)
