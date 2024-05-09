5171b7a0918fe8f0cb3ee93a81e68d812139fb8b
GabrielMoraes4299 <135380543+GabrielMoraes4299@users.noreply.github.com>
2024-05-09 09:28:41 -0300

    Adicionando os inserts

---
+++ INSERTS tb_categorias - gg_tech_hub.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_categorias (nome_categoria) VALUES
+('Smartphones'),
+('Notebooks'),
+('Câmeras'),
+('Tablets'),
+('Smartwatches'),
+('Acessórios de Áudio'),
+('Monitores e TVs'),
+('Consoles de Jogos'),
+('Impressoras'),
+('Roteadores e Redes');
