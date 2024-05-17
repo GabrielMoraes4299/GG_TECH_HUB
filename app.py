bc7b8da8a823a59d2f3e204384ba2e22d51b3f35
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-05-17 14:14:27 -0300

    Criação de uma variação do título de cada página de acordo com uma variável predeterminada.

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app.secret_key = "capivara"
 
 @app.route("/")
 def pg_inicial():
-    return render_template("cabecalho-rodape.html")
+    return render_template("cabecalho-rodape.html", campo_titulo="GG TECH HUB")
 
 @app.route("/cadastro-login")
 def pg_cadastrar_form():
-    return render_template("cadastro-login.html")
+    return render_template("cadastro-login.html", campo_titulo="Cadastro ou Login")
 
 @app.route("/cadastro", methods=["POST"])
 def pg_cadastro():
